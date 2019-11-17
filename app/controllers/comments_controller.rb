2f5028d14d055e44d1d53865b73911fd4b95e169
twkhjl <twkhjl@gmail.com>
2019-11-17 02:47:37 -0800

    add comment delete function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,14 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article),notice:"new comment successfully added"
   end
   
+  def destroy
+    @article=Article.find_by(id: params[:article_id])
+    @comment=@article.comments.find_by(id: params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+
   private
   def comment_params
     params.require(:comment).permit(:commenter,:body)
